a59d5b158495840bf77a98a16d6f2692acfc373c
Zay Maung Maung Myint <zaymaungmaungmyint@advent-soft.com>
2024-05-03 01:07:04 +0700

    Feat
    - add web project sample

---
+++ serving-web-content/src/test/java/dec/zay/example/servingwebcontent/ServingWebContentApplicationTests.java
@@ -0,0 +1,13 @@
+package dec.zay.example.servingwebcontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServingWebContentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
